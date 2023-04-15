35afc44e606cd838ce8e5ad0f08c82fc739a8ad0
henry.choi <oreobro@naver.com>
2023-04-15 18:24:26 +0900

    날라갈뻔

--- WWDC/SwiftChallenge.swiftpm/NavationViews/ScrollViewScene.swift
+++ WWDC/SwiftChallenge.swiftpm/NavationViews/ScrollViewScene.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ScrollViewScene: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(0...4 , id: \.self) { i in
+                    NavigationLink(destination: MatchColorScene(indexNum: i))
+                    {
+                        VStack {
+                            Image(info.image[i])
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 240, height: 320)
+                                .padding()
+                            Text(info.title[i])
+                                .padding(.top)
+                        }
+                    }.navigationTitle("Gallery")
+                }
+            }
+        }
     }
 }
 
