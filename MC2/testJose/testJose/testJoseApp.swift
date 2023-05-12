e973c554eb1ab95fa9b091a36df85612f58396c7
henry.choi <oreobro@naver.com>
2023-05-12 15:37:34 +0900

    WWDC Final Commit

---
+++ MC2/testJose/testJose/testJoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  testJoseApp.swift
+//  testJose
+//
+//  Created by 최민규 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct testJoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoverContentView()
+        }
+    }
+}
